7a08c6f96cd309e25204ea1ab676fe51b92b75ab
Eugene Min <e.v.min@icloud.com>
2016-04-13 13:41:28 +1100

    Check if table exists before updating

--- src/Cacheable.php
+++ src/Cacheable.php
@@ -6,88 +6,98 @@ use Cache;
 
 trait Cacheable
 {
-	
-	protected static $suffix = '_all';
-	protected static $cacheName;
-	
-	public function __construct(array $attributes = [])
-	{
-		parent::__construct($attributes);
-		static::observeEvents();
-	}
-	
-	/**
-	 * Get all cached items of given Eloquent Model.
-	 * 
-	 * @access public
-	 * @static
-	 * @return Collection
-	 */
-	public static function getCached($name = null)
-	{
-		self::initialize($name);
-		
-		if (!Cache::has(self::$cacheName))
-			self::updateCache();
-		
-		return Cache::get(self::$cacheName);
-	}
-	
-	/**
-	 * Update all given Eloquent Model items in cache.
-	 * 
-	 * @access public
-	 * @static
-	 * @return bool
-	 */
-	public static function updateCache($name = null)
-	{
-		self::initialize($name);
-		Cache::forever(self::$cacheName, static::all());
-		return true;
-	}
-	
-	/**
-	 * Drop class Caches.
-	 * 
-	 * @access public
-	 * @static
-	 * @return bool
-	 */
-	public static function dropCache($name = null)
-	{
-		self::initialize($name);
-		Cache::forget(self::$cacheName);
-		return true;
-	}
-	
-	/**
-	 * Initialize class.
-	 * 
-	 * @access protected
-	 * @static
-	 * @return void
-	 */
-	protected static function initialize($name = null)
-	{
-		if (!is_null($name))
-			self::$cacheName = static::class . $name;
-		else
-			self::$cacheName = static::class . self::$suffix;
-	}
-	
-	/**
-	 * Observe model events.
-	 * 
-	 * @access protected
-	 * @static
-	 * @return void
-	 */
-	protected static function observeEvents()
-	{
-		static::saved(function ($item) {
-			static::updateCache();
-		});
-	}
-	
+    protected static $suffix = '_all';
+    protected static $cacheName;
+    
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        static::observeEvents();
+    }
+    
+    /**
+     * Get all cached items of given Eloquent Model.
+     * 
+     * @access public
+     * @static
+     * @return Illuminate\Support\Collection
+     */
+    public static function getCached($name = null)
+    {
+        self::initialize($name);
+        
+        if (!Cache::has(self::$cacheName)) {
+            if (!self::updateCache()) {
+                return collect();
+            }
+        }
+        
+        return Cache::get(self::$cacheName);
+    }
+    
+    /**
+     * Update all given Eloquent Model items in cache.
+     * 
+     * @access public
+     * @static
+     * @return bool
+     */
+    public static function updateCache($name = null)
+    {
+        self::initialize($name);
+        
+        $instance = new static;
+        
+        if (!Schema::hasTable($instance->getTable())) {
+            return false;
+        }
+        
+        Cache::forever(self::$cacheName, static::all());
+        
+        return true;
+    }
+    
+    /**
+     * Drop class Caches.
+     * 
+     * @access public
+     * @static
+     * @return bool
+     */
+    public static function dropCache($name = null)
+    {
+        self::initialize($name);
+        Cache::forget(self::$cacheName);
+        return true;
+    }
+    
+    /**
+     * Initialize class.
+     * 
+     * @access protected
+     * @static
+     * @return void
+     */
+    protected static function initialize($name = null)
+    {
+        if (!is_null($name)) {
+            self::$cacheName = static::class . $name;
+        } else {
+            self::$cacheName = static::class . self::$suffix;
+        }
+    }
+    
+    /**
+     * Observe model events.
+     * 
+     * @access protected
+     * @static
+     * @return void
+     */
+    protected static function observeEvents()
+    {
+        static::saved(function ($item) {
+            static::updateCache();
+        });
+    }
 }
